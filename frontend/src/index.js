4eaaffde27f8e729c4db3600951f7e6a6a29b87e
syedfquadri <sd.falahuddin@gmail.com>
2023-07-10 17:22:18 +0530

    added basename and bowerser router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+  } from "react-router-dom";
 import App from './App';
 import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router basename='/rcts/speech-collection/'>
+        {/* <Switch>
+            <Route path="/" component={App} />
+        </Switch> */}
+        <App />
+    </Router>
 );
 
