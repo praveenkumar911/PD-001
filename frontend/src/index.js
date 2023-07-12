839067c736f34de7edd5b27cc0e8d0d66b7a522a
syedfquadri <sd.falahuddin@gmail.com>
2023-07-12 22:11:58 +0530

    basename

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {
     BrowserRouter as Router,
     Route,
-    Switch,
+    Routes,
   } from "react-router-dom";
 import App from './App';
 import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router basename='/rcts/speech-collection'>
-        {/* <Switch>
-            <Route path="/" component={App} />
-        </Switch> */}
-        <App />
+        <Routes>
+            <Route exact path="/" element={<App />} />
+        </Routes>
+        {/* <App /> */}
     </Router>
 );
-
